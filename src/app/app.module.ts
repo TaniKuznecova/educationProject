ee9ba77c881cee755d23d16d904972f2ba63073b
Tatyana_Kuznetsova <kuznecovata@iris-retail.ru>
2023-12-07 21:46:23 +0500

    дашборд

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { CartPageComponent } from './components/cart-page/cart-page.component';
+import { LikedPageComponent } from './components/liked-page/liked-page.component';
+import { TopComponent } from './components/top/top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    CartPageComponent,
+    LikedPageComponent,
+    TopComponent,
   ],
   imports: [
     BrowserModule,
